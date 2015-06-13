c213c346250b1aac04bc67d5dda8ef72c31b86c5
anicolasgar <a.nicolasgar@gmail.com>
2015-06-13 16:48:16 -0300

    subo repository y service pais

---
+++ src/PagoElectronico/PagoElectronico.Services/PaisService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PagoElectronico.Repositories;
+using PagoElectronico.Entities;
+
+namespace PagoElectronico.Services
+{
+    public class PaisService
+    {
+        public IEnumerable<Pais> getByMayorIngresosEgresos(DateTime fechaDesde, DateTime fechaHasta)
+        {
+            PaisRepository paisRepo = new PaisRepository();
+            return paisRepo.getByMayorIngresosEgresos(fechaDesde, fechaHasta);
+        }
+
+    }
+}
