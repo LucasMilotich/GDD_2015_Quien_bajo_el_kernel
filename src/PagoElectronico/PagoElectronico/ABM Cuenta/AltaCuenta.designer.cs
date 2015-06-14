53458ebbde1787837b01f8fbf215bcb94709c7c9
santiagoGin <santi.ginabreda@hotmail.com>
2015-06-14 02:05:37 -0300

    agregando archivos de abm de cuentas

---
+++ src/PagoElectronico/PagoElectronico/ABM Cuenta/AltaCuenta.designer.cs
@@ -0,0 +1,174 @@
+﻿namespace PagoElectronico.ABM_Cuenta
+{
+    partial class AltaCuenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCuenta = new System.Windows.Forms.TextBox();
+            this.txtMoneda = new System.Windows.Forms.TextBox();
+            this.lblCuenta = new System.Windows.Forms.Label();
+            this.lblPais = new System.Windows.Forms.Label();
+            this.lblMoneda = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.lblTipoCuenta = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtCuenta
+            // 
+            this.txtCuenta.Location = new System.Drawing.Point(107, 31);
+            this.txtCuenta.Name = "txtCuenta";
+            this.txtCuenta.Size = new System.Drawing.Size(159, 20);
+            this.txtCuenta.TabIndex = 0;
+            // 
+            // txtMoneda
+            // 
+            this.txtMoneda.Enabled = false;
+            this.txtMoneda.Location = new System.Drawing.Point(107, 112);
+            this.txtMoneda.Name = "txtMoneda";
+            this.txtMoneda.Size = new System.Drawing.Size(72, 20);
+            this.txtMoneda.TabIndex = 2;
+            // 
+            // lblCuenta
+            // 
+            this.lblCuenta.AutoSize = true;
+            this.lblCuenta.Location = new System.Drawing.Point(16, 35);
+            this.lblCuenta.Name = "lblCuenta";
+            this.lblCuenta.Size = new System.Drawing.Size(81, 13);
+            this.lblCuenta.TabIndex = 5;
+            this.lblCuenta.Text = "Número Cuenta";
+            // 
+            // lblPais
+            // 
+            this.lblPais.AutoSize = true;
+            this.lblPais.Location = new System.Drawing.Point(16, 76);
+            this.lblPais.Name = "lblPais";
+            this.lblPais.Size = new System.Drawing.Size(27, 13);
+            this.lblPais.TabIndex = 6;
+            this.lblPais.Text = "Pais";
+            // 
+            // lblMoneda
+            // 
+            this.lblMoneda.AutoSize = true;
+            this.lblMoneda.Location = new System.Drawing.Point(16, 118);
+            this.lblMoneda.Name = "lblMoneda";
+            this.lblMoneda.Size = new System.Drawing.Size(46, 13);
+            this.lblMoneda.TabIndex = 7;
+            this.lblMoneda.Text = "Moneda";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(16, 157);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(80, 13);
+            this.lblFecha.TabIndex = 8;
+            this.lblFecha.Text = "Fecha Apertura";
+            // 
+            // lblTipoCuenta
+            // 
+            this.lblTipoCuenta.AutoSize = true;
+            this.lblTipoCuenta.Location = new System.Drawing.Point(16, 199);
+            this.lblTipoCuenta.Name = "lblTipoCuenta";
+            this.lblTipoCuenta.Size = new System.Drawing.Size(65, 13);
+            this.lblTipoCuenta.TabIndex = 9;
+            this.lblTipoCuenta.Text = "Tipo Cuenta";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(147, 245);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(119, 33);
+            this.btnGuardar.TabIndex = 10;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(107, 152);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(182, 20);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(107, 71);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 12;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(107, 192);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 13;
+            // 
+            // AltaCuenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 304);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.lblTipoCuenta);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.lblMoneda);
+            this.Controls.Add(this.lblPais);
+            this.Controls.Add(this.lblCuenta);
+            this.Controls.Add(this.txtMoneda);
+            this.Controls.Add(this.txtCuenta);
+            this.Name = "AltaCuenta";
+            this.Text = "Alta de Cuenta";
+            this.Load += new System.EventHandler(this.AltaCuenta_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCuenta;
+        private System.Windows.Forms.TextBox txtMoneda;
+        private System.Windows.Forms.Label lblCuenta;
+        private System.Windows.Forms.Label lblPais;
+        private System.Windows.Forms.Label lblMoneda;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label lblTipoCuenta;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
