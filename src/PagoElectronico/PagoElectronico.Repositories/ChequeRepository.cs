0ba8b21302ab621308a15b7879656c7af2168c36
anicolasgar <a.nicolasgar@gmail.com>
2015-07-11 22:12:40 -0300

    Retiros

--- src/PagoElectronico/PagoElectronico.Repositories/ChequeRepository.cs
+++ src/PagoElectronico/PagoElectronico.Repositories/ChequeRepository.cs
@@ -8,5 +8,30 @@ namespace PagoElectronico.Repositories
 {
     class ChequeRepository : BaseRepository<Cheque>
     {
+        
+        public override IEnumerable<Cheque> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Cheque Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Insert(Cheque entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(Cheque entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Delete(Cheque entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- src/PagoElectronico/PagoElectronico.Services/ChequeService.cs
+++ src/PagoElectronico/PagoElectronico.Services/ChequeService.cs
@@ -5,7 +5,8 @@ using System.Text;
 
 namespace PagoElectronico.Services
 {
-    class ChequeService
+    public class ChequeService
     {
+
     }
 }
